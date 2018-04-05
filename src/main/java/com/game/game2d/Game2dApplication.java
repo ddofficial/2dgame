47f103b356650df0837630e7c8a6e45b36a56231
Dominik Kunze <dd_official@web.de>
2018-04-05 14:14:13 +0200

    first commit

---
+++ src/main/java/com/game/game2d/Game2dApplication.java
@@ -0,0 +1,12 @@
+package com.game.game2d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game2dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Game2dApplication.class, args);
+	}
+}
